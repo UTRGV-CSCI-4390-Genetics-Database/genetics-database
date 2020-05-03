5fb47529ae41376ec5520cd14e5bf90ca6aafeab
Brian Bowman <seeker14491@gmail.com>
2020-05-02 22:33:49 -0500

    Refactor app.js

--- app.js
+++ app.js
@@ -1,85 +1,100 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
-const path = require('path');
-const fsPromises = require('fs').promises;
-const Client = require('pg').Client;
 const bodyParser = require('body-parser');
+const express = require('express');
+const fsPromises = require('fs').promises;
+const path = require('path');
+const { Client } = require('pg');
+const util = require('util');
+
+const PORT = process.env.PORT || 5000;
+
+async function saveSchema(schema) {
+  try {
+    await fsPromises.writeFile(`${__dirname}/public/data.js`, `const data = ${util.inspect(schema, { maxArrayLength: null })};`);
+    console.log('Schema saved');
+  } catch (e) {
+    console.error(`Failed to save the schema: ${e}`);
+    throw e;
+  }
+}
+
+async function connectToDatabase() {
+  const client = new Client({ connectionString: process.env.DATABASE_URL });
+  try {
+    await client.connect();
+  } catch (e) {
+    console.error(`Failed to connect to database: ${e}`);
+    throw e;
+  }
+
+  console.log('Connected to database');
+  return client;
+}
+
+async function executeQuery(client, query) {
+  const results = await client.query(query);
+  return results.rows;
+}
+
+const app = express();
 app.use(bodyParser.json());
-//app.use(express.json());
-app.use(express.urlencoded({extended: true})) 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-var util = require('util');
-async function saveFile(obj) {
-    try {
-        await fsPromises.writeFile(`${__dirname }/public/data.js`, 'var data = ' + util.inspect(obj, {maxArrayLength: null}));
-        console.log('File saved');
-    }
-    catch(e) {
-        console.error(`Failed to save a file ${e}`);
-    }
-}
-const client = new Client({
-    user: 'genetics_user',
-    password: '***REMOVED***',
-    host: '***REMOVED***',
-    database: 'genetics'
+
+let client;
+
+app.get('/home', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
 });
-async function connect() {
-    try {
-        await client.connect();
-        console.log('Connected to Data Base');
-    }
-    catch(e) {
-        console.error(`Failed to connect ${e}`);
-    }
-}
-async function read(text){
-    try {
-    const results = await client.query(text);
-    return results.rows;
-    }
-    catch(e){
-        console.error(`Problem - ${e}`);
-        return(e);
-    }
-}
-app.get('/home', function(req, res){
-    res.sendFile(`${__dirname }/public/index.html`);})
-app.get('/about', function(req, res){
-    res.sendFile(`${__dirname }/public/about.html`);})
-app.get('/search', function(req, res){
-    res.sendFile(`${__dirname }/public/search.html`);})
-app.get('/post', function(req, res){
-    res.sendFile(`${__dirname }/public/post.html`);})
-app.get('/update', function(req, res){
-    res.sendFile(`${__dirname }/public/update.html`);})
-app.get('/delete', function(req, res){
-    res.sendFile(`${__dirname }/public/delete.html`);})
-app.post('/results', async function(req, res){
-    console.log(req.body.obj.request);
-    var table = await read(req.body.obj.request);
-    console.log(table);
-    res.send(table);});
-app.post('/schema', async function(req, res){
-    var obj = req.body.obj;
-    newObj = {};
-    for (let [key, value] of Object.entries(obj)){
-       newArr = [];
-       for(i=0; i <obj[key].length; i++ ){
-            if(!(key == "individuals" || key == "blood_samples" || key == "category_individuals") && obj[key][i]['column_name'] == "subject_id"){}
-            else{
-                minObj = {}
-                minObj[obj[key][i]['column_name']] = obj[key][i]['data_type'];
-                newArr.push(minObj);
-            }
-       }
-       newObj[key]=newArr;
-    }
-    await saveFile(newObj);
-    res.send("Schema sucesfully updated");
+
+app.get('/about', (req, res) => {
+  res.sendFile(`${__dirname}/public/about.html`);
 });
-connect();
-app.listen(PORT, function(){
-console.log(`server started on port ${PORT}`);
-})
+
+app.get('/search', (req, res) => {
+  res.sendFile(`${__dirname}/public/search.html`);
+});
+
+app.get('/post', (req, res) => {
+  res.sendFile(`${__dirname}/public/post.html`);
+});
+
+app.get('/update', (req, res) => {
+  res.sendFile(`${__dirname}/public/update.html`);
+});
+
+app.get('/delete', (req, res) => {
+  res.sendFile(`${__dirname}/public/delete.html`);
+});
+
+app.post('/results', async (req, res) => {
+  console.log(req.body.obj.request);
+  const rows = await executeQuery(client, req.body.obj.request);
+  console.log(rows);
+  res.send(rows);
+});
+
+app.post('/schema', async (req, res) => {
+  const { obj } = req.body;
+  const newSchema = {};
+  for (const [key, value] of Object.entries(obj)) {
+    const newArr = [];
+    for (let i = 0; i < value.length; i += 1) {
+      if (key === 'individuals' || key === 'blood_samples' || key === 'category_individuals' || value[i].column_name !== 'subject_id') {
+        const minObj = {
+          [value[i].column_name]: value[i].data_type,
+        };
+        newArr.push(minObj);
+      }
+    }
+    newSchema[key] = newArr;
+  }
+  await saveSchema(newSchema);
+  res.send('Schema successfully updated');
+});
+
+(async () => {
+  client = await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+  });
+})();
